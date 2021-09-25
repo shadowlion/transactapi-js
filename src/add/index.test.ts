40088f93900e7ca76bde2e0286f33c8e299c1558
Justin Chiou <chiou.kai@gmail.com>
2021-09-25 14:34:42 -0400

    refactor: remove sanity check

--- src/add/index.test.ts
+++
@@ -1,5 +0,0 @@
-import add from ".";
-
-it("adds two numbers together", () => {
-  expect(add(1, 2)).toEqual(3);
-});
--- src/add/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Adds two numbers together.
- * @params n1: number
- * @params n2: number
- * @returns number
- */
-export default (n1: number, n2: number): number => {
-  return n1 + n2;
-}
