f58c6a932cad8b6be5fde5a068bfe3032c868fd3
Justin Chiou <chiou.kai@gmail.com>
2021-10-04 11:13:01 -0400

    fix: add endpoints to index

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export { default as createExternalAccount } from "./createExternalAccount";
 export { default as createTrade } from "./createTrade";
 export { default as externalFundMove } from "./externalFundMove";
 export { default as getAccount } from "./getAccount";
+export { default as getAllTrades } from "./getAllTrades";
+export { default as getOffering } from "./getOffering";
