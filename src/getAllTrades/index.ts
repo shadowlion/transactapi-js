42c1539158669662a227fcef0bde83985e4b67a5
Justin Chiou <chiou.kai@gmail.com>
2021-10-05 17:29:55 -0400

    fix: documentation referencing

--- src/getAllTrades/index.ts
+++ src/getAllTrades/index.ts
@@ -5,7 +5,7 @@ import { GetAllTradesRequest, GetAllTradesResponse } from "./types";
  * Get all trades for a given client.
  * @link https://api.norcapsecurities.com/admin_v3/documentation?mid=MzU4
  * @param {string} clientID - Transact API Client ID
- * @param {string} clientID - Transact API Developer Key
+ * @param {string} developerAPIKey - Transact API Developer Key
  * @returns {string} statusCode - API Status Code
  * @returns {string} statusDesc - API Status Description
  * @returns {Array} TradeDetails - Array of Trade Details
