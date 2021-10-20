15a0dd2f7f5fd515164648ac5018742a6820890c
Justin Chiou <chiou.kai@gmail.com>
2021-10-20 16:48:38 -0400

    fix: conditionally run through HTTP methods

--- src/base.ts
+++ src/base.ts
@@ -7,7 +7,7 @@ export const BASE_URL = "https://api.norcapsecurities.com/tapiv3/index.php/v3/";
  * @param {string} method - HTTP method
  * @param {string} endpoint - API endpoint, referenced from documentation
  * @param {object} payload - Object of request parameters, referenced from documentation
- * @returns AxiosResponse
+ * @return AxiosResponse
  */
 export async function baseApi<Payload, Response>(
   method: Method,
@@ -15,12 +15,12 @@ export async function baseApi<Payload, Response>(
   payload: Payload,
 ): Promise<Response> {
   try {
-    const config: AxiosRequestConfig = {
-      method: method,
-      url: BASE_URL + endpoint,
-      data: payload,
-    }
-    const res: AxiosResponse<Response> = await axios(config);
+    let res: AxiosResponse<Response>;
+    const url = BASE_URL + endpoint;
+    const config: AxiosRequestConfig<Payload> = { data: payload };
+    if (method === "GET") res = await axios.get(url);
+    if (method === "POST") res = await axios.post(url, config);
+    if (method === "PUT") res = await axios.put(url, config);
     return res.data;
   } catch (error) {
     throw new Error(error.message);
