ef0ade185c2067d0b0ce23ca0ef34dc56a767074
Justin Chiou <chiou.kai@gmail.com>
2021-10-20 15:55:59 -0400

    fix: change axios to use generic config

--- src/base.ts
+++ src/base.ts
@@ -1,22 +1,26 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse, Method } from "axios";
 
 export const BASE_URL = "https://api.norcapsecurities.com/tapiv3/index.php/v3/";
 
 /**
  * Base API call to Transact API
- * @param endpoint API endpoint, referenced from documentation
- * @param payload Object of parameters, referenced from documentation
- * @returns JSON response
+ * @param {string} method - HTTP method
+ * @param {string} endpoint - API endpoint, referenced from documentation
+ * @param {object} payload - Object of request parameters, referenced from documentation
+ * @returns AxiosResponse
  */
 export async function baseApi<Payload, Response>(
+  method: Method,
   endpoint: string,
   payload: Payload,
 ): Promise<Response> {
   try {
-    const res: AxiosResponse<Response> = await axios.post(
-      BASE_URL + endpoint,
-      payload,
-    );
+    const config: AxiosRequestConfig = {
+      method: method,
+      url: BASE_URL + endpoint,
+      data: payload,
+    }
+    const res: AxiosResponse<Response> = await axios(config);
     return res.data;
   } catch (error) {
     throw new Error(error.message);
