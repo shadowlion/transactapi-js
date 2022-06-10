73d451ab448ad64510067e8eae606142ad20b8c2
Rob Brown <rwbrown@roadrunner.com>
2022-06-10 06:30:26 -1000

    updating code comments

--- src/createParty/index.ts
+++ src/createParty/index.ts
@@ -3,8 +3,21 @@ import { Domicile } from "./enums";
 import { CreatePartyRequest, CreatePartyResponse } from "./types";
 
 /**
- * This method is used to create a party
+ * This method is used to create a party.
  * @link https://api.norcapsecurities.com/admin_v3/documentation?mid=MQ==
+ * @param {string} clientID - Transact API Client ID
+ * @param {string} developerAPIKey - Transact API Developer Key
+ * @param {string} domicile - Is the party a U.S. Citizen, U.S. Resident, or non-resident
+ * @param {string} firstName - Party's First Name
+ * @param {string} lastName - Party's Last Name
+ * @param {string} dob - The Party's Date of Birth (MM-DD-YYYY)
+ * @param {string} primCountry - The country in which the Party's physical address resides
+ * @param {string} primAddress1 - The Party's physical address, line 1
+ * @param {string} primAddress2 - The Party's physical address, line 2
+ * @param {string} primCity - The city of the Party's physical address
+ * @param {string} primState - Party's residence state ( must be two letter code only ) (It is optional for non-us parties)
+ * @param {string} primZip - Zip code
+ * @param {string} emailAddress - Party's contact email address
  */
 
 export default async function ({
@@ -16,6 +29,7 @@ export default async function ({
   dob,
   primCountry,
   primAddress1,
+  primAddress2,
   primCity,
   primState,
   primZip,
@@ -31,6 +45,7 @@ export default async function ({
         dob,
         primCountry,
         primAddress1,
+        primAddress2,
         primCity,
         primState,
         primZip,
--- src/createParty/types.ts
+++ src/createParty/types.ts
@@ -11,6 +11,7 @@ export interface CreatePartyRequest {
   dob: string;
   primCountry: string;
   primAddress1: string;
+  primAddress2: string;
   primCity: string;
   primState: string;
   primZip: string;
@@ -28,21 +29,3 @@ interface PartyDetail {
   KYCstatus: string;
   AMLstatus: string;
 }
-
-
-/*
-clientID  yes string  Transact API Client ID
-developerAPIKey yes string  Transact API Developer Key
-domicile  yes conditional Is the party a U.S. Citizen, U.S. Resident, or non-resident
-firstName yes string  Party's First Name
-lastName  yes string  Party's Last Name
-dob yes string  The Party's Date of Birth (MM-DD-YYYY)
-primCountry yes string  The country in which the Party's physical address resides
-primAddress1  yes string  The Party's physical address, line 1
-primCity  yes string  The city of the Party's physical address
-primState yes string  Party's residence state ( must be two letter code only ) (It is optional for non-us parties)
-primZip yes string  Zip code
-emailAddress  yes string  Party's contact email address
-
-
-*/
